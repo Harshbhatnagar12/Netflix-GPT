2daa37656e434f1983b7d8ec1c01579ed0de1e35
Harshbhatnagar12 <harshbhatnagar20222@gmail.com>
2024-11-09 17:29:55 +0530

    Sign In / Sign Up Form // Lofin Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold bg-slate-700" >
-     HElloooo
-    </div>
-  );
+  return <Body/>;
+
+
 }
 
 export default App;
---
+++ src/components/Browse.js
@@ -0,0 +1,8 @@
+const Browse = () => {
+  return (
+    <div className='text-4xl bg-slate-500'>
+     Browser
+    </div>
+  )
+}
+export default Browse
